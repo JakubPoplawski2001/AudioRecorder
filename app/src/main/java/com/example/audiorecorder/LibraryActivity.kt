997ea8d93f33c58888daff65686454c0a66a53eb
Jakub <wrx75670@student.wsb.wroclaw.pl>
2024-11-29 21:05:40 +0100

    LibraryList

--- app/src/main/java/com/example/audiorecorder/LibraryActivity.kt
+++ app/src/main/java/com/example/audiorecorder/LibraryActivity.kt
@@ -1,6 +1,7 @@
 package com.example.audiorecorder
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +16,18 @@ class LibraryActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true);
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> return super.onOptionsItemSelected(item)
         }
     }
 }
